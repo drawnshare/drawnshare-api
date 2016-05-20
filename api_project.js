4cfd64b91738877b9976eee8b0b8d29f4609f211
half-shell <michel.maubert.pro@gmail.com>
2016-05-20 20:45:43 +0200

    [apidoc] Generate and update API documentation

--- api_project.js
+++ api_project.js
@@ -1,12 +1,12 @@
 define({
   "name": "drawnshare-api",
-  "version": "0.4.0",
+  "version": "1.0.1",
   "description": "Small API used in the drawn n' share project.",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-05-20T17:21:22.088Z",
+    "time": "2016-05-20T18:45:02.231Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
